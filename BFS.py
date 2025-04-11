b359aca0a339120c9590c335abc1b53b20f4ccee
DungZan <142530655+DungZan@users.noreply.github.com>
2025-04-11 08:55:57 +0700

    hot fix

--- BFS.py
+++ BFS.py
@@ -1,5 +1,3 @@
-import queue
-
 from tabulate import tabulate
 
 
@@ -36,13 +34,14 @@ def Search(trongso, ke, start, end):
                 print("Tìm kiếm thành công")
                 row.append([u + str(trongso[u]), "TTKT-Dừng", None])
                 return tabulate(row, headers=["Phát triển TT", "Trạng thái kề", "Danh sách L"]) + "\nĐường đi là: " + " => ".join(i + str(trongso[i]) for i in path)
-
+            
             for v in ke.get(u, []):
                 new_path = path + [v]
                 L.append((v, new_path))
                 L.sort(key=lambda x: trongso.get(x[0]))
             row.append([u+str(trongso[u]), ", ".join([i + str(trongso[i]) for i in ke[u]]),", ".join(i1 + str(trongso[i1]) for i1,i2 in L)])
-
+        
+        return "Không tìm thấy đường đi"
 
 def printFile(filename, content):
         file = open(filename, "w", encoding="utf-8")
@@ -51,6 +50,6 @@ def printFile(filename, content):
 
 if __name__ == '__main__':
     trongso, ke, start, end = readfile("BestFistSearch\inputbfs1.txt")
-    print(Search(trongso, ke, start, end))
+    #print(Search(trongso, ke, start, end))
     printFile("BestFistSearch\output1.txt",Search(trongso, ke, start, end))
 
